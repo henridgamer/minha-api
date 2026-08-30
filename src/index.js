81cfa8765a71568d97c3690ea2c1a31c6b5bb211
Henri Ribeiro <henridelucamacedo@gmail.com>
2026-08-30 13:36:16 -0300

    Corrigindo servidor Express

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 module.exports = app;
